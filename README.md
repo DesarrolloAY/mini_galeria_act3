8152d855c886b13e378ef988d21f926c88052199
DesarrolloAY <ymollob@gmail.com>
2025-08-28 23:40:24 -0500

    inicio

---
+++ README.md
@@ -0,0 +1,3 @@
+# mini_galeria_act3
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mini_galeria_act3/pages/home_page.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: HomePage()));
+  }
+}
